d992a333372df61adbf4e2e98b95ab83228208ee
Saxe-fr <anton.saxe.fr@gmail.com>
2022-07-19 15:53:37 +0200

    V.6 : Another opti and some modification

--- calculator.py
+++ calculator.py
@@ -1,9 +1,9 @@
  
 #*
 # ! Fonction :
-# ? F1 = Calcul à partir des données fournies par l'utilisateur le coéf mutliplicateur, puis le taux d'évolution qu'il return
+# ? F1 = Calcul a partir des données fournie par l'utilisateur le coéf mutliplicateur, puis le taux d'évolution qu'il return
 #*
- 
+
 def taux_et_coefmutlipli(x1, x2, x3):
     sx1 = sens(x1)
     sx2 = sens(x2)
@@ -17,18 +17,15 @@ def taux_et_coefmutlipli(x1, x2, x3):
     return (round(t,3))
 
 #*
-# ? F2 = Calcul le taux réciproque à partir d'un taux donné par l'utilisateur 
+# ? F2 = Calcul le taux réciproque a partir d'un taux donné par l'utilisateur 
 #*
 
-def taux_recipr(t, t2):
-    if t > 0:
-        cmr = 1/(1+(t/100))
-        tr = cmr - 1
-    else :
-        t = t * -1
-        cmr = 1/(1-(t/100))
-        tr = cmr - 1
+def taux_recipr(t):
+    try:
+        tr = 1/tr
         return (round(tr,3))
+    except:
+        print("En l’absence de précisions supplémentaires sur l’axiomatique utilisée, selon le second théorème de Gödel, la proposition est indécidables")
 
 #*
 # ? F3 = Demande des évolutions et utilisation de la F1
@@ -52,29 +49,28 @@ def sens(x):
     elif x < 0:
         return "une baisse"
     else:
-        return "nul"
-
+        return "nulle"
 
 #*
 # ! Don des réponses 
 #* wdyw = what do you whant
 #*
 
-wdyw = input("\nQue souhaitez vous calculer ? Faites votre choix parmis les posibilités ci-dessous :\n\n1) taux d'évolution\n2) taux réciproque\n3) taux d'évolution puis taux réciproque\n4) taux d'évolution et taux réciproque\n\n")
+wdyw = input("\nQue souhaitez vous calculez ? Faites votre choix parmis les posibilité ci-dessous :\n\n1) taux d'évolution\n2) taux reciproque\n3) taux d'évolution puis taux reciproque\n4) taux d'évolution et taux reciproque\n\n")
 if wdyw == "1":
     ter = t() 
 elif wdyw == "2":
-    t1 = float(input("Faites le choix du coeficient multiplicateur (en pourcentage) : "))
+    t1 = float(input("Faites le choix du premier coeficien multiplicateur (en pourcentage) : "))
     tr = taux_recipr(t1)
-    print("\nLe taux réciproque est aproximativement à " + str(tr) + ", en pourcentage " + str(tr*100) + "%")
+    print("\nLe taux reciproque est aproximativement à " + str(tr) + ", en pourcentage " + str(tr*100) + "%")
 elif wdyw == "3":
     ter = t()
     tr = taux_recipr(ter)
-    print("\nLe taux réciproque est aproximativement à " + str(tr) + ", en pourcentage " + str(tr*100) + "%")
+    print("\nLe taux reciproque est aproximativement à " + str(tr) + ", en pourcentage " + str(tr*100) + "%")
 elif wdyw == "4":
     t()
-    t1 = float(input("Faites le choix du coeficient multplicateur (en pourcentage) : "))
+    t1 = float(input("Faites le choix du premier coeficien multplicateur (en pourcentage) : "))
     tr = taux_recipr(t1)
-    print("\nLe taux réciproque est aproximativement à " + str(tr) + ", en pourcentage " + str(tr*100) + "%")
+    print("\nLe taux reciproque est aproximativement à " + str(tr) + ", en pourcentage " + str(tr*100) + "%")
 else:
-    print("Veuillez vérifier la saisie du bon numéro attribué au(x) calcul(s) choisi(s).")
+    print("Veuillez verifiez la saisie du bon numéro attribuer au(x) calcul(s) choisi(s).")
